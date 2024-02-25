3d295a15202a35e74fe0c191243b7c0c73098c38
Patrick Santos <patrick.sg.santos@gmail.com>
2024-02-25 11:33:25 -0800

    create of trips done

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -8,12 +8,18 @@ class TripsController < ApplicationController
   end
 
   def new
+    if current_user.nil?
+      redirect_to new_user_session_path, alert: "You need to sign in first"
+      return
+    end
+
     @trip = Trip.new
   end
 
   def create
     @trip = Trip.new(trip_params)
     @trip.owned_by = current_user
+    binding.irb
     if @trip.save
       redirect_to trip_path(@trip)
     else
@@ -33,7 +39,7 @@ class TripsController < ApplicationController
   private
 
   def trip_params
-    params.require(:trip).permit(:title, :date, :duration, :destiny, :user_id)
+    params.require(:trip).permit(:title, :date, :duration, :place)
   end
 
   def set_trip
